fb660f5284e7d6ff361ffe5d0090275bba70b720
highlander <bithighlander@gmail.com>
2019-08-03 17:57:00 -0600

    works, but ugly as shit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,121 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+
+    <textarea @paste="onPaste"></textarea>
+
+    <div class="control">
+      <input v-model="word1" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word2" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word3" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word4" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word5" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word6" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word7" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word8" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word9" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word10" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word11" placeholder="edit me" @paste="onPaste">
+    </div>
+    <div class="control">
+      <input v-model="word12" placeholder="edit me" @paste="onPaste">
+    </div>
+<!--    <h1>Finds</h1>-->
+<!--    <div v-for="find in finds">-->
+<!--      <input @paste="onPaste" v-model="find.value">-->
+<!--    </div>-->
+<!--    <button @click="addFind">-->
+<!--      New Find-->
+<!--    </button>-->
+<!--    <pre>{{ $data | json }}</pre>-->
+
   </div>
 </template>
 
 <script>
+  const bip39 = require('bip39')
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      word1:'',
+      word2:'',
+      word3:'',
+      word4:'',
+      word5:'',
+      word6:'',
+      word7:'',
+      word8:'',
+      word9:'',
+      word10:'',
+      word11:'',
+      word12:'',
+    };
+  },
+  watch: {
+    word1: function (val) {
+      //validate word
+      console.log("val: ",val)
+
+      //edge case, if whole seed
+      //val = val.split("\n")
+      val = val.split(" ")
+      console.log("val2: ",val)
+      this.word1 = val[0]
+
+
+    },
+    word2: function (val) {
+
+    }
+  },
+  methods: {
+    addFind: function () {
+      this.finds.push({ value: '' });
+    },
+    onPaste (evt) {
+      console.log('on paste', evt)
+      console.log('on paste', evt.clipboardData.getData('Text'))
+      let seedWords = evt.clipboardData.getData('Text')
+      seedWords = seedWords.split("\n")
+      console.log('seedWords', seedWords)
+
+
+      this.word2 = seedWords[1]
+      this.word3 = seedWords[2]
+      this.word4 = seedWords[3]
+      this.word5 = seedWords[4]
+      this.word6 = seedWords[5]
+      this.word7 = seedWords[6]
+      this.word8 = seedWords[7]
+      this.word9 = seedWords[8]
+      this.word10 = seedWords[9]
+      this.word11 = seedWords[10]
+      this.word12 = seedWords[11]
+      this.word1 = seedWords[0]
+      return true
+    }
   }
 }
 </script>
